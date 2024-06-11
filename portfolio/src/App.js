f171baeea5e3d29f2ce9cfc55f196aa765001ca3
DevanshiB29 <bhusaridevanshi@gmail.com>
2024-06-11 17:19:28 +0530

    added footer section

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -5,6 +5,7 @@ import Intro from './components/Introduction/Intro';
 import Skills from './components/Skills/Skills';
 import Works from './components/Works/Works';
 import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
 function App() {
   return (
     <div className="App">
@@ -13,6 +14,7 @@ function App() {
       <Skills/>
       <Works/>
       <Contact/>
+      <Footer/>
     </div>
   );
 }
---
+++ portfolio/src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+        Copyright &#198; 2024 Devanshi Bhusari. All right received.
+    </footer>
+  )
+}
+
+export default Footer
