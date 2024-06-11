bda301fa469db4120228304fb293b9a54fc3c84d
DevanshiB29 <bhusaridevanshi@gmail.com>
2024-06-11 15:55:04 +0530

    added contact me section

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/NavBar/Navbar';
 import Intro from './components/Introduction/Intro';
 import Skills from './components/Skills/Skills';
 import Works from './components/Works/Works';
+import Contact from './components/Contact/Contact';
 function App() {
   return (
     <div className="App">
@@ -11,6 +12,7 @@ function App() {
       <Intro/>
       <Skills/>
       <Works/>
+      <Contact/>
     </div>
   );
 }
--- portfolio/src/components/Contact/Contact.js
+++ portfolio/src/components/Contact/Contact.js
@@ -1,17 +1,30 @@
 import React from 'react'
-
+import './Contact.css';
+import linkedin from '../../assets/linkedin.png'
+import email from '../../assets/mail.png'
+import github from '../../assets/github.jpg'
+import leetcode from '../../assets/leetcode.png'
 const Contact = () => {
   return (
-   <section className="contactPage">
+   <section id="contactPage">
         <div id="contact">
             <h1 className="contactPageTitle">Contact Me</h1>
             <span className="contactDesc">Please fill out the form below to discuss any work opportunities</span>
             <form className="contactForm">
                 <input type="text" className="name" placeholder='Your Name'/>
                 <input type="email" className="email" placeholder='Your Email'/>
-                
+                <textarea className="msg" name="message" rows="5" placeholder='Your Message'></textarea>
+                <button type='submit' value='Send' className="submitBtn">Submit</button>
+                <div className="links">
+                    <img src={linkedin} alt='linkedin' className="link"/>
+                    <img src={email} alt='email' className="link"/>
+                    <img src={github} alt='github' className="link"/>
+                    <img src={leetcode} alt='leetcode' className="link"/>
+
+                </div>
             </form>
         </div>
+    </section>   
   )
 }
 
