08bca5764b2ff354d9170b4dbf4871ad820b314d
DevanshiB29 <bhusaridevanshi@gmail.com>
2024-06-12 10:02:36 +0530

    added email service

--- portfolio/src/components/Contact/Contact.js
+++ portfolio/src/components/Contact/Contact.js
@@ -1,18 +1,38 @@
-import React from 'react'
+import React,{useRef} from 'react'
 import './Contact.css';
 import linkedin from '../../assets/linkedin.png'
 import email from '../../assets/mail.png'
 import github from '../../assets/github.jpg'
 import leetcode from '../../assets/leetcode.png'
+import emailjs from '@emailjs/browser';
 const Contact = () => {
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs
+          .sendForm('service_ikggemj', 'template_bjqgh6l', form.current, {
+            publicKey: 's9epzOBRRH252EOl7',
+          })
+          .then(
+            () => {
+              console.log('SUCCESS!');
+              e.target.reset();
+              alert('Email sent!');
+            },
+            (error) => {
+              console.log('FAILED...', error.text);
+            },
+          );
+      };
   return (
-   <section id="contactPage">
+    <section id="contactPage">
         <div id="contact">
             <h1 className="contactPageTitle">Contact Me</h1>
             <span className="contactDesc">Please fill out the form below to discuss any work opportunities</span>
-            <form className="contactForm">
-                <input type="text" className="name" placeholder='Your Name'/>
-                <input type="email" className="email" placeholder='Your Email'/>
+            <form className="contactForm" ref={form} onSubmit={sendEmail}>
+                <input type="text" className="name" placeholder='Your Name' name='your_name'/>
+                <input type="email" className="email" placeholder='Your Email' name='your_email'/>
                 <textarea className="msg" name="message" rows="5" placeholder='Your Message'></textarea>
                 <button type='submit' value='Send' className="submitBtn">Submit</button>
                 <div className="links">
--- portfolio/src/components/NavBar/Navbar.js
+++ portfolio/src/components/NavBar/Navbar.js
@@ -9,12 +9,15 @@ const Navbar = () => {
     <nav className="navbar">
         <img src={logo} alt="Logo" className='logo'/>
         <div className="desktopMenu">
-            <Link className="desktopMenuListItem">Home</Link>
-            <Link className="desktopMenuListItem">About</Link>
-            <Link className="desktopMenuListItem">Portfolio</Link>
+            <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">Home</Link>
+            <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">Skills</Link>
+            <Link activeClass='active' to='works' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">Works</Link>
+            
            
         </div>
-        <button className="desktopMenuBtn">
+        <button className="desktopMenuBtn" onClick={()=>{
+            document.getElementById('contact').scrollIntoView({behavior:'smooth'});
+        }}>
             <img src={contactImg} alt="" className="desktopMenuImg" />Contact Me
         </button>
         </nav>
